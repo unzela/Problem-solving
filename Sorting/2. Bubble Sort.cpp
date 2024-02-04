eec515f486d5a78db2c99bf73b49aa97db5583ca
Unzela Inamdar <51193316+unzela@users.noreply.github.com>
2024-02-04 15:42:04 +0000

    Sorting

--- Sorting/2. Bubble Sort.cpp
+++ Sorting/2. Bubble Sort.cpp
@@ -11,6 +11,25 @@
 #include <vector>
 using namespace std;
 
+void bubble_sort(int arr[], int n) {
+    // bubble sort
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "After Using bubble sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
 int main() {
   vector<int> arr{3, 6, 1, 4, 2, 9};
   int n = arr.size();
--- Sorting/3. Insertion Sort.cpp
+++ Sorting/3. Insertion Sort.cpp
@@ -5,6 +5,24 @@
 #include <vector>
 using namespace std;
 
+void insertion_sort(int arr[], int n) {
+    for (int i = 0; i <= n - 1; i++) {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+
+    cout << "After Using insertion sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
 int main() {
   vector<int> arr{3, 1, 7, 4, 9, 2};
   int n = arr.size();
